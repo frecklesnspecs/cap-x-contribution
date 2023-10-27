2a0d1369a7654eb483720095f7c3f9af6092f28f
Clarissa <144487978+frecklesnspecs@users.noreply.github.com>
2023-10-26 22:24:35 -0700

    Second Task# T346641
    Created a Bug model and created two bugs through Django Admin per Task# T346641

--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Bug(models.Model):
+
+    BUG_TYPE = [
+        ("ERROR", "Error"),
+        ("NEW FEATURE", "New Feature"),
+        ("INTEGRATION", "Integration"),
+        ("OTHER", "Other")
+    ]
+
+    STATUS = [
+        ("TO DO", "To Do"),
+        ("IN PROGRESS", "In Progress"),
+        ("DONE", "Done"),
+    ]
+
+    description = models.TextField()
+    bug_type = models.CharField(max_length=200, choices=BUG_TYPE)
+    report_date = models.DateTimeField("Date Reported")
+    status = models.CharField(max_length=200, choices=STATUS)
+
+    def __str__(self):
+        return self.description
+
+    def was_reported_recently(self):
+        return self.report_date >= timezone.now() - datetime.timedelta(days=1)
