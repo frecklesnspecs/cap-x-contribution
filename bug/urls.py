12d8afc5d107dd40e0994cb01aa0a22645ba1a71
Clarissa <144487978+frecklesnspecs@users.noreply.github.com>
2023-10-29 13:44:35 -0700

    Third Task# T347255
    Created views for List of Bugs (index.html), Bug Information (info.html), and registering a new bug into the database (new.html).

--- bug/urls.py
+++ bug/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "bug"
 urlpatterns = [
+    # ex: /bug/
     path("", views.index, name="index"),
+    # ex: /bug/5/
+    path("<int:bug_id>/", views.info, name="info"),
+    # ex: /bug/new/
+    path("new/", views.new, name="new"),
+    # ex: /bug/save/
+    path("save/", views.save, name="save")
 ]
--- bug/views.py
+++ bug/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from .models import Bug
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bug index.")
+    bug_list = Bug.objects.order_by("-report_date")
+    context = {"bug_list": bug_list}
+    return render(request, "bug/index.html", context)
+
+
+def info(request, bug_id):
+    bug = get_object_or_404(Bug, pk=bug_id)
+    return render(request, "bug/info.html", {"bug": bug})
+
+
+def new(request):
+    return render(request, "bug/new.html", {"bug_types": Bug.BUG_TYPE})
+
+def save(request):
+   if request.POST["description"] == "":
+      return render(
+          request,
+          "bug/new.html", {
+              "error_message": "You did not describe the bug.",
+              "bug_types": Bug.BUG_TYPE,
+          },
+      )
+   else:
+    b = Bug(description=request.POST["description"],
+            bug_type=request.POST["BUG_TYPE"],
+            report_date=timezone.now(),
+            status="TO DO")
+    b.save()
+    return HttpResponseRedirect(reverse("bug:info", args=(b.id,)))
+
+   
